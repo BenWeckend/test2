c436571c7eae3df15e04a9d5689be58929f122c9
BenWeckend <54399339+BenWeckend@users.noreply.github.com>
2023-05-31 13:04:56 +0200

    Add files via upload

---
+++ aufg4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace aufg4
+{
+    class Unsername
+            {
+                private string _name;  
+                public string Name    
+                {
+                    get => _name;
+                    set => _name = value;
+                }
+            }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           while (true)
+            {
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "new_user" :
+                    var User = new Unsername();
+                    Console.Write("create new User: ");
+                    User.Name = Console.ReadLine();  
+                    System.Console.Write(User.Name);
+                  //case "user_logout" :
+                    break;
+                default: 
+                    break;
+            }
+            }
+            
+        }
+        
+        
+    }
+    
+}
+
